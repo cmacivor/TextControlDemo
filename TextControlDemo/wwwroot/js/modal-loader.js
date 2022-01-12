11a59f063ef7d8dac5f981d10b4a0e1b312fb1c1
Craig MacIvor <craig.macivor@udig.com>
2022-01-11 21:20:36 -0500

    begin moving to separate file

---
+++ TextControlDemo/wwwroot/js/modal-loader.js
@@ -0,0 +1,42 @@
+﻿function SaveToController(DocumentName) {
+    TXTextControl.saveDocument(TXTextControl.streamType.InternalUnicodeFormat,
+        function (e) {
+            console.log(DocumentName)
+            console.log(e.data)
+            var serviceURL = "/Home/SaveTemplate";
+
+            $.ajax({
+                type: "POST",
+                url: serviceURL,
+                contentType: 'application/json',
+                data: JSON.stringify({
+                    DocumentName: DocumentName,
+                    BinaryDocument: e.data
+                }),
+                success: successFunc,
+                error: errorFunc
+            });
+
+            function successFunc(data, status) {
+                var modal = document.getElementById("myModal");
+                modal.style.display = "none";
+                alert("Document has been saved successfully.");
+            }
+
+            function errorFunc() {
+                alert('Error');
+            }
+        });
+}
+
+
+function saveFile() {
+    var fileName = document.getElementById("txtFileName").value;
+
+    SaveToController(fileName);
+}
+
+
+function openModal() {
+    alert('test');
+}
