19080a3b26c05ac926cf859948956d579fe5c265
Craig MacIvor <craig.macivor@udig.com>
2022-01-12 19:59:03 -0500

    move these over

--- TextControlDemo/wwwroot/js/modal-loader.js
+++ TextControlDemo/wwwroot/js/modal-loader.js
@@ -36,6 +36,49 @@ function saveFile() {
     SaveToController(fileName);
 }
 
+function loadFile() {
+    var fileName = document.getElementById("txtFileName").value;
+    LoadFromController(fileName);
+}
+
+
+
+// load the document from the controller and load
+// it into the TXTextControl.Web
+function LoadFromController(DocumentName) {
+    var serviceURL = "/Home/LoadTemplate";
+    $.ajax({
+        type: "POST",
+        url: serviceURL,
+        contentType: 'application/json',
+        data: JSON.stringify({
+            DocumentName: DocumentName
+        }),
+        success: successFunc,
+        error: errorFunc
+    });
+    function successFunc(data, status) {
+        TXTextControl.isTrackChangesEnabled = true;
+        TXTextControl.showSideBar(TXTextControl.SideBarType.TrackChanges, 1);
+        TXTextControl.loadDocument(TXTextControl.streamType.InternalUnicodeFormat, data);
+    }
+    function errorFunc() {
+        alert('Error');
+    }
+
+}
+
+function trackedChangeSwitchEventHandler() {
+    var checkBox = document.getElementById("chkTrackChanges");
+    if (checkBox.checked) {
+        //alert('its checked!');
+        TXTextControl.isTrackChangesEnabled = true;
+        TXTextControl.showSideBar(TXTextControl.SideBarType.TrackChanges, 1);
+    } else {
+        alert('its not checked');
+    }
+}
+
 
 function openModal() {
     alert('test');
